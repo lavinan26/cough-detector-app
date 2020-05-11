d5111140c3992e393b751379f9023036628d43d1
MohamedAfham <afhamaflal9@gmail.com>
2020-05-11 23:28:25 +0530

    testing for CNN model

--- src/app/main.py
+++ src/app/main.py
@@ -2,26 +2,29 @@ import sys
 sys.path.append("./")
 from threading import Thread
 import logging
-from modules.soundRecorder import SoundRecorder
-from modules.soundReceiver import SoundReceiver
-from modules.responder import Responder
-from modules.processor import Processor
-from recordSound.soundRecorder import record
-from process.preProcessor import preProcess
-from process.detector import infer
-from respond.responder import respond
+from tensorflow.keras.models import load_model
+from src.modules.soundRecorder import SoundRecorder
+from src.modules.soundReceiver import SoundReceiver
+from src.modules.responder import Responder
+from src.modules.processor import Processor
+from src.recordSound.soundRecorder import record
+from src.process.preProcessor import PreProcessing
+from src.process.detector import infer
+from src.respond.responder import respond
+
+preprocess = PreProcessing()
 
 #Load model in the below line
-model = "load model"
+model = load_model("src\\model\\model_CNN.h5")
 logging.basicConfig(format='%(asctime)s-%(levelname)s - %(message)s', level=logging.INFO)
 receiver = SoundReceiver(logging = logging,overlap_duration = 2, sample_rate = 22050)
 source = SoundRecorder(process = record, sink = receiver,sample_rate = 22050, recording_duration = 5,logging = logging, overlap_duration = 2)
 responder = Responder(logging = logging, respond = respond)
-processor = Processor(source = receiver, sink = responder, pre_processor = preProcess, detector = infer, logging = logging, model = model)
+processor = Processor(source = receiver, sink = responder, pre_processor = preprocess, detector = infer, logging = logging, model = model)
 source.setDaemon = True
 processor.setDaemon = True
 source.start()
 processor.start()
-while True:
-    a = 2
+#while True:
+#    a = 2
     #logging.info(receiver.queue)
