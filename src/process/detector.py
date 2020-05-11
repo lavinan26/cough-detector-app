208ad004da4c412689f62657b4ad710027f3e831
MohamedAfham <afhamaflal9@gmail.com>
2020-05-11 23:27:49 +0530

    model prediction

--- src/process/detector.py
+++ src/process/detector.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 def infer(signal,logging,model):
-    #START
-    result = "cough"
-    #END
+    signal = np.expand_dims(signal,axis=0)
+    prediction = model.predict(x = signal,batch_size = 1)
+    prediction = np.round(prediction)
+    if prediction:
+        result = "cough"
+    else:
+        result = "non_cough"
     #return result as either "cough" or "non_cough"
     return result
