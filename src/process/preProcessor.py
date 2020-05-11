17e204aeb19fe08efbf0e6a399bf0a07420fce24
MohamedAfham <afhamaflal9@gmail.com>
2020-05-11 23:27:11 +0530

    spectrogram function

--- src/process/preProcessor.py
+++ src/process/preProcessor.py
@@ -1,8 +1,24 @@
-#import modules
+import numpy as np
+from scipy.signal import spectrogram
 
-def preProcess(signal,logging):
-    #START
-    spectogram = signal
-    #END
-    #Return the spectogram as an array which can be directly fed into CNN
-    return spectogram
+
+class PreProcessing():
+    def __init__(self,sampling_rate=22050):
+        self.sampling_rate = sampling_rate
+
+    def __call__(self,audio_array,logging):
+        audio_array = np.squeeze(audio_array)
+        spectrum = self.spect(audio_array)
+        normal_spectrum = self.normalize_spectrum(spectrum)
+        return normal_spectrum
+
+    def spect(self,audio):
+        sampling_rate = self.sampling_rate
+        NFFT = 512
+        f, t, Sxx = spectrogram(x=audio,fs=sampling_rate,window=np.hamming(NFFT),nfft=NFFT,noverlap=int(NFFT/3),nperseg=NFFT,mode='magnitude')
+        spectrum = 20*np.log10(Sxx)
+        spectrum = spectrum.ravel().reshape(257,321,1)
+        return spectrum
+
+    def normalize_spectrum(self,spectrum):
+        return (spectrum - np.mean(spectrum))/np.std(spectrum)
